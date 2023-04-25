702f1702ed3e8bd9b51a15a7c7f6d490c74db251
Vardhan Dintakurthi <vardhan.performs@gmail.com>
2023-04-25 11:36:36 -0500

    initial commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xc36ca0DCE6294E5358e83d794eb7139B607EAaB3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
